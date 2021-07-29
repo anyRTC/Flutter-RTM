91e1a155c4c2c48faa2be5dc261a89418974e539
liuxiaozhong <>
2021-07-29 10:03:29 +0800

    update android sdk

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -57,9 +57,7 @@ class _MyAppState extends State<MyApp> {
 
   void _createClient() async {
     _client =
-    await ARRtmClient.createInstance("YOUR APP ID");
-    // var params= '{"Cmd": "ConfPriCloudAddr","ServerAdd": "1.1.1.1","Port":7080}';
-    // _client.setParameters(params);
+    await ARRtmClient.createInstance("YOUR APPID");
     _client.onMessageReceived = (ARRtmMessage message, String peerId) {
       _log("Peer msg: " + peerId + ", msg: " + message.text);
     };
